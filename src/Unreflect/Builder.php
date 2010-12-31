893d58ed7577335dc838727eb954e778aa7d753b
Alexander Reece <AlReece45@gmail.com>
2010-12-31 16:16:31 -0600

    Added Builder interface

---
+++ src/Unreflect/Builder.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Unreflect Library
+ * Copyright 2010 Alexander Reece
+ * Licensed under: GNU Lesser Public License 2.1 or later
+ */
+/**
+ * @author Alexander Reece <AlReece45@gmail.com>
+ * @copyright 2010 (c) Alexander Reece
+ * @license http://www.opensource.org/licenses/lgpl-2.1.php
+ * @package Unreflect
+ */
+
+/**
+ * Base interface for the builder
+ */
+interface Unreflect_Builder
+{
+	function export();
+}
